f7f8ffbf9a56fe7e20a2481a1a521b864e2d6e0c
feras <fdxd@fdxd-l.mynet>
2020-09-29 18:35:05 +0300

    Saving plot

--- src/predict.py
+++ src/predict.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 from PIL import Image
 import torch
 
@@ -9,20 +10,23 @@ from utils.dataset import Loader
 from model.unet import UNet
 
 data_folder = "../data/"
-model_path = "../model/model.pth"
+model_path = "../model/"
+# model_name = "RMSprop_100e_0001_model"
+model_name = "model"
+save_path = model_path + model_name + '/'
 
 
 def predict(net):
 
-    testset = Loader(data_folder)
+    testset = Loader(data_folder, test=True)
     test_loader = DataLoader(
         testset, batch_size=1, shuffle=True, num_workers=8, pin_memory=True
     )
     net.eval()
-    for i in test_loader:
+    print(len(testset))
+    for idx, i in enumerate(test_loader):
         with torch.no_grad():
             input = i["image"]
-
             input = input.to(device=device, dtype=torch.float32)
 
             output = net(input)
@@ -32,34 +36,44 @@ def predict(net):
 
             full_mask = probs.squeeze().cpu().numpy()
 
-
-            _, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True)
-            ax1.imshow(full_mask[0,:,:].squeeze())
-            ax2.imshow(full_mask[1,:,:].squeeze())
-            ax3.imshow(full_mask[2,:,:].squeeze())
-            plt.show()
+            fig, (ax0, ax1, ax2, ax3, ax4) = plt.subplots(1, 5, sharey=True,figsize=(14,6))
+            ax0.set_title('Input Image')
+            ax0.imshow(input.cpu().squeeze(0).permute(1,2,0).numpy())
+            ax1.set_title('Background Class')
+            ax1.imshow(full_mask[0, :, :].squeeze())
+            ax2.set_title('Neuron Class')
+            ax2.imshow(full_mask[1, :, :].squeeze())
+            ax3.set_title('Dendrite Class')
+            ax3.imshow(full_mask[2, :, :].squeeze())
 
             full_mask = np.argmax(full_mask, 0)
 
             img = mask_to_image(full_mask)
 
-            plt.imshow(img)
+            ax4.set_title('Predicted Mask')
+            ax4.imshow(img)
             plt.show()
-            # input_array = input.squeeze().detach().numpy()
-            # output_array = output.argmax(2) * 255
-            # input_img = Image.fromarray(input_array)
-            # output_img = Image.fromarray(output_array.astype(dtype=np.uint16)).convert("L")
-            # input_img.show()
-            # output_img.show()
+            #fig.savefig(save_path + str(idx + 1) + '.png')
+            #img.save(str(idx) + '.png')
     return full_mask
 
+
 def mask_to_image(mask):
-    return Image.fromarray((mask * 255).astype(np.uint8)).convert('RGB')
+    return Image.fromarray((mask * 255).astype(np.uint8)).convert("RGB")
+
+def get_concat_h(im1, im2):
+    dst = Image.new('RGB', (im1.width + im2.width, im1.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (im1.width, 0))
+    return dst
 
 if __name__ == "__main__":
     net = UNet(n_channels=3, n_classes=3)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     net.to(device=device)
-
-    net.load_state_dict(torch.load(model_path, map_location=device))
+    try:
+        os.mkdir(save_path)
+    except:
+        pass
+    net.load_state_dict(torch.load(model_path + model_name + '.pth', map_location=device))
     predict(net)
